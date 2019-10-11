938620f04ab9b7893a840dfc7ce8609585d8439b
J-Anne Carlson <carlsonjc3@J-Annes-MBP.hsd1.wa.comcast.net>
2019-10-11 16:14:46 -0700

    added  specific argV values

--- app/liri.js
+++ app/liri.js
@@ -2,21 +2,37 @@ require("dotenv").config();
 
 var keys = require("./keys.js")
 var Spotify = require('node-spotify-api');
-
 var spotify = new Spotify(keys.spotify);
 
+var action = process.argv[2];
+var title = process.argv.splice(3).join(" ");
+
+
 // Example request for our reference
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    var names = data.artists.map(artist => artist.name);
-    names.join(', ');
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
+var spotifyTHIS = function(song){
+  spotify.search({ type: 'track', query:song}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data.tracks.items[0].album.artists[0].name); 
+  console.log(data.tracks.items[0].name); 
+  console.log(data.tracks.items[0].preview_url);
+  console.log(data.tracks.items[0].album.name);
   });
 
+} 
 
-  function doSomething(withSomething) {
-     return withSomething + " I DID IT!";
-  }
+if (action === "spotify-this-song"){
+
+  spotifyTHIS(title)
+}
+
+//OMDB API Key *** 
+//BandsInTowns Key
+
+//PLACE keys in Key folder so they are encrypted
+//Require API key to connect to javascript (file pathway to keys)
+//Create variable expression with function expression for concerts for BIT
+//Axios library for bands in town event pull data and OMDB
+//Look though data object, write argument values for data retreived 
