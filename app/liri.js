c2dc37de10649e4b7c52aad6addf46e9cd91be09
J-Anne Carlson <carlsonjc3@J-Annes-MacBook-Pro.local>
2019-10-15 20:27:21 -0700

    added BandsInTown

--- app/liri.js
+++ app/liri.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 
 var keys = require("./keys.js")
 var Spotify = require('node-spotify-api');
+const axios = require('axios');
 var spotify = new Spotify(keys.spotify);
 
 var action = process.argv[2];
-var title = process.argv.splice(3).join(" ");
+var input = process.argv.splice(3).join(" ");
 
 
 // Example request for our reference
@@ -23,40 +24,54 @@ var spotifyTHIS = function (song) {
     console.log(data.tracks.items[0].preview_url);
     console.log(data.tracks.items[0].album.name);
   });
-
 }
+var bandsintownTHIS = function (artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  
+    axios.get(queryURL).then(
+      function(response) {
+        var jsonData = response.data;
+  
+        console.log(jsonData);
+   })
+  }
+
+
+  
+
+// COMMAND --> node liri.js concert-this <artist/band name here>
 // **ACTION = SPOTIFY THIS SONG COMMAND** //
 if (action === "spotify-this-song") {
-  if (title === "") {
-    title = "Ace of base - the sign";
+  if (input === "") {
+    input = "Ace of base - the sign";
   }
   spotifyTHIS(title)
 }
 // **ACTION = CONCERT THIS** //
 if (action === "concert-this") {
-  if (title === "") {
-    title = "BLANK";
+  if (input === "") {
+    input = "BLANK";
   }
-  spotifyTHIS(concert)
+  bandsintownTHIS(input)
 }
-// **ACTION = MOVIE THIS** //
-if (action === "movie-this") {
-  if (title === "") {
-    title = "BLANK";
-  }
-  spotifyTHIS(concert)
-}
-// **ACTION = DO WHAT IT SAYS** //
-if (action === "do-what-it-says") {
-  if (title === "") {
-    title = "BLANK";
-  }
-  spotifyTHIS(concert)
-}
-// **LOG TO TEXT** //
-if (action === output) {
-  if (output === log text) {
-  }
+// // **ACTION = MOVIE THIS** //
+// if (action === "movie-this") {
+//   if (title === "") {
+//     title = "BLANK";
+//   }
+//   spotifyTHIS(concert)
+// }
+// // **ACTION = DO WHAT IT SAYS** //
+// if (action === "do-what-it-says") {
+//   if (title === "") {
+//     title = "BLANK";
+//   }
+//   spotifyTHIS(concert)
+// }
+// // **LOG TO TEXT** //
+// if (action === output) {
+//   if (output === log text) {
+  // }
 
 
 
